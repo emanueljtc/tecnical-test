abff3b587c4e7841e43c14d3aa77ece11bc0fb1b
Emanuel Torres Clemente <emanueljtc@gmail.com>
2023-12-20 16:05:27 -0400

    feat: add vercel speedInsights

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import { Home } from "../pages";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   return (
     <>
